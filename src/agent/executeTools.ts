2b5da7e59ddede8e4436be5d9bbf0920597bcf37
Mario Pavlov <marioppavlov@gmail.com>
2026-04-11 09:07:32 +0300

    add execute tools for all available tools

---
+++ src/agent/executeTools.ts
@@ -0,0 +1,20 @@
+import { tools } from "./tools/index.ts";
+
+export type Toolname = keyof typeof tools;
+
+export const executeTools = async (name: string, args: any) => {
+  const tool = tools[name as Toolname];
+
+  if (!tool) {
+    return "Unknown tool, this does not exist.";
+  }
+
+  const execute = tool.execute;
+
+  if (!execute) {
+    return "This is not a registered tool.";
+  }
+
+  const result = await execute(args, { toolCallId: "", messages: [] });
+  return String(result);
+};
